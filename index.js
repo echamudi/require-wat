1e1ed37eb011f8beaf6a087566ac5256f96ed4d7
Ezzat Chamudi <chamudi.git@outlook.com>
2019-10-08 21:36:07 +0300

    Add support for import object

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const { readFileSync } = require('fs')
 const wabt = require('wabt')();
 
-const requireWat = async (watFile) => {
+const requireWat = async (watFile, importObject = {}) => {
     const parsedWat = wabt.parseWat(watFile, readFileSync(watFile, "utf8"));
     const buffer = parsedWat.toBinary({}).buffer;
     const module = await WebAssembly.compile(buffer);
-    const instance = await WebAssembly.instantiate(module);
+    const instance = await WebAssembly.instantiate(module, importObject);
 
     return instance.exports;
 };
