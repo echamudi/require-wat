f926670afe548cbced5d301a67513ac8f9673d57
Ezzat Chamudi <chamudi.git@outlook.com>
2019-10-08 21:51:43 +0300

    Add global

--- test/test.js
+++ test/test.js
@@ -3,8 +3,8 @@ const assert = require('assert');
 const path = require('path');
 
 it('js can load wat function', async function() {
-    const watFile = path.join(__dirname, '/calc.wat');
 
+    const watFile = path.join(__dirname, '/calc.wat');
     const { add, sub } = await requireWat(watFile);
 
     assert.deepStrictEqual(add(5,5), 10);
@@ -16,8 +16,8 @@ it('js can load wat function', async function() {
 });
 
 it('wat can load js function', async function() {
-    const watFile = path.join(__dirname, '/volume.wat');
 
+    const watFile = path.join(__dirname, '/volume.wat');
     const { cubeVolume } = await requireWat(watFile, {
         calculate: {
             positivePower3: function(n) {
@@ -31,3 +31,31 @@ it('wat can load js function', async function() {
     assert.deepStrictEqual(cubeVolume(10), 1000);
     assert.deepStrictEqual(cubeVolume(-10), 0);
 });
+
+// No global in node js
+
+// it('wat can access global', async function() {
+
+//     const global = WebAssembly.Global({value: "i32", mutable: true}, 0);
+
+//     const watFile = path.join(__dirname, '/global.wat');
+//     const { getGlobal, incGlobal } = await requireWat(watFile);
+
+//     global.value++;
+//     global.value++;
+
+//     assert.deepStrictEqual(global.value, 2);
+
+//     incGlobal();
+//     incGlobal();
+//     incGlobal();
+
+//     assert.deepStrictEqual(global.value, 5);
+
+//     global.value++;
+//     incGlobal();
+
+//     assert.deepStrictEqual(global.value, 7);
+//     assert.deepStrictEqual(getGlobal(), 7);
+
+// });
